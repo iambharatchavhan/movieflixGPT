10f1ea1308c8b3b8ae9798e04894f37b81f25d56
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-08-31 20:12:37 +0530

    project setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border-2 border-solid border-red-500 ">
+   <p>Welcome to my movie application </p>  
     </div>
   );
 }
