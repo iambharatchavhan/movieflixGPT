af9d826c2c45924b3e463bf8a5d24af8727d177a
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-09-01 16:52:49 +0530

    project ready to expand

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <div className="border-2 border-solid border-red-500 ">
-   <p>Welcome to my movie application </p>  
+   <p>Welcome to my movie application</p>  
     </div>
   );
 }
