babbc638af5fa853e231e34f554af45026a1123e
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-09-04 22:54:59 +0530

    mobile ui_login_page ready

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,25 +4,34 @@ import Header from './Header'
 
 const Login = () => {
   return (
-    <div className='bg-black border border-yellow-700 w-full h-screen'>
+    <div className='bg-black  w-full h-screen'>
 <Header/>
     
-     <div className='flex justify-center items-center h-[100vh] border border-white' >
+     <div className='flex justify-center items-center h-[100vh]' >
        
        <img src={IMG_BG} alt="" srcset="" className='hidden backdrop-brightness-95 bg-white/30' />
        <div className='absolute top-12 h-full'>
         <div className=' h-full w-screen'>
-        <h1 className='text-3xl font-semibold text-white mt-5 ml-4'>Sign In</h1>
+        <h1 className='text-[1.9rem] font-bold text-white mt-3 ml-4'>Sign In</h1>
           <form action="" className=' h-full flex flex-col justify-start items-center mt-5'>
-           <div className='border border-blue-700 h-2/3 flex flex-col w-full items-center gap-3 p-4'>
-            <input type="text" placeholder='Email or phone number' className='p-3 w-full rounded-sm bg-[#333333]'/>
-            <input type="password" placeholder='Password' className='p-3 w-full rounded-sm bg-[#333333]'/> 
-            <input type="submit" value="Sign In" className='text-white p-3 w-full rounded-md bg-[#E50914] mt-5'/> 
+           <div className=' h-2/1 flex flex-col w-full items-center gap-4 p-4'>
+            <input type="text" placeholder='Email or phone number' className='p-3.5 w-full rounded-md bg-[#333333]'/>
+            <input type="password" placeholder='Password' className='p-3.5 w-full rounded-md bg-[#333333]'/> 
+            <button className='text-white font-bold p-3.5 w-full rounded-md bg-[#E50914] mt-5'>Sign In</button>
            </div>
-            <div  className='text-white'>
+            <div  className='text-white w-full flex justify-between px-4'>
+              <div className=''>
              <label htmlFor="remember">Remember me</label> <input type="checkbox" name='remember'/>
-             <a href="www.google.com">Need help ?</a>
+
+              </div>
+              <div>
+             <a href="www.google.com" >Need help?</a>
+             
+              </div>
+             
+
             </div>
+            <a href="www.google.com" className='text-white  w-full px-2 py-2 text-md'><span className='text-gray-400 px-2 '>New to Netflix?</span> Sign up now</a>
           </form>
         </div>
        </div>
